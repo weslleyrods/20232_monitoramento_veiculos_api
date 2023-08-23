3371e90d34fa30e6e53105d671e72a3399686120
Weslley Rodrigues <weslley.rdgs@gmail.com>
2023-08-23 18:44:53 +0000

    feat(directions): rota entre dois pontos

--- src/maps/directions/directions.controller.ts
+++ src/maps/directions/directions.controller.ts
@@ -1,15 +1,16 @@
-import { Controller, Query } from '@nestjs/common';
+import { Controller, Get, Query } from '@nestjs/common';
 import { DirectionsService } from './directions.service';
 
 @Controller('directions')
 export class DirectionsController {
 
-  constructor(private directionService: DirectionsService){}
+  constructor(private directionsService: DirectionsService){}
 
+  @Get()
   getDirections(
     @Query('originId') originId: string,
-    @Query('directionId') directionId: string
+    @Query('destinationId') destinationId: string
     ){
-      return this.directionService.getDirections(originId, directionId);
-    };
+      return this.directionsService.getDirections(originId, destinationId);
+  };
 }
